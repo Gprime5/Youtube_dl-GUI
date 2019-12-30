6b7a3d0d1a1cd8290f29f07300d14e21b021aabc
Gprime <gprime@hotmail.co.uk>
2019-12-30 21:09:50 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,33 @@
-from tkinter import *
-from tkinter import ttk
-import json
 from io import BytesIO
+from tkinter import Tk, ttk, Menu, StringVar, Entry
+import json
 
-import requests
 from PIL import Image, ImageTk
+import requests
 
-from downloader import Preview, Downloader
+import downloader
 
-class Popup_menu(Menu):
-    def __init__(self, parent):
+class PopupMenu(Menu):
+    def __init__(self, parent, *functions):
         super().__init__(parent, tearoff=0)
 
-        self.add_command(label="\tCut", command=self.cut)
-        self.add_command(label="\tCopy", command=self.copy)
-        self.add_command(label="\tPaste", command=self.paste)
+        self.widget = None
 
-    def cut(self):
-        self.entry.cut()
+        for func in functions:
+            self.add_command(label=f"\t{func.capitalize().replace('_', ' ')}", command=lambda f=func:getattr(self.widget, f)())
 
-    def copy(self):
-        self.entry.copy()
-
-    def paste(self):
-        self.entry.paste()
-
-    def set_entry(self, entry):
-        self.entry = entry
+    def post(self, widget, x, y):
+        self.widget = widget
+        super().post(x, y)
 
 class MyEntry(Entry):
     def __init__(self, parent, **kwargs):
         self.var = StringVar()
         self.default = ""
 
-        super().__init__(parent, textvariable=self.var, bg="SystemButtonFace", relief="flat", **kwargs)
+        super().__init__(parent, textvariable=self.var, **kwargs)
 
         self.bind("<Escape>", self.reset)
-        self.bind("<Button-3>", self.popup)
 
     def cut(self):
         if self.selection_present():
@@ -44,7 +35,8 @@ class MyEntry(Entry):
             self.delete(*sorted((self.index("anchor"), self.index("insert"))))
 
     def copy(self):
-        self.clipboard_append(self.selection_get())
+        if self.selection_present():
+            self.clipboard_append(self.selection_get())
 
     def paste(self):
         if self.selection_present():
@@ -55,14 +47,13 @@ class MyEntry(Entry):
         self.var.set(text)
         self.default = text
 
+    def get(self):
+        return self.var.get()
+
     def reset(self, args):
         self.var.set(self.default)
 
-    def popup(self, args):
-        self.master.menu.set_entry(self)
-        self.master.menu.post(args.x_root, args.y_root)
-
-class Previewer_frame(Frame):
+class PreviewFrame(ttk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
 
@@ -70,22 +61,20 @@ class Previewer_frame(Frame):
 
         self.image = self._blank_image = ImageTk.PhotoImage(Image.new("RGB", (128, 72), (240, 240, 240)))
 
-        self.thumbnail_frame = Label(self, image=self.image)
-        self.thumbnail_frame.grid(padx=5, pady=5, rowspan=5)
+        self.thumbnail_frame = ttk.Label(self, image=self.image)
+        self.thumbnail_frame.grid(rowspan=3)
 
-        Label(self, text="URL: ").grid(column=1, row=0, sticky="ne")
+        ttk.Label(self, text="URL: ").grid(column=1, row=0, sticky="ne")
         self.url_entry = MyEntry(self, state="readonly")
-        self.url_entry.grid(column=2, row=0, sticky="nwe")
+        self.url_entry.grid(column=2, row=0, sticky="we")
 
-        Label(self, text="Title: ").grid(column=1, row=1, sticky="ne")
+        ttk.Label(self, text="Title: ").grid(column=1, row=1, sticky="ne")
         self.title_entry = MyEntry(self)
-        self.title_entry.grid(column=2, row=1, sticky="nwe")
+        self.title_entry.grid(column=2, row=1, sticky="we")
 
-        Label(self, text="Uploader: ").grid(column=1, row=2, sticky="ne")
+        ttk.Label(self, text="Uploader: ").grid(column=1, row=2, sticky="ne")
         self.uploader_entry = MyEntry(self)
-        self.uploader_entry.grid(column=2, row=2, sticky="nwe")
-
-        self.menu = Popup_menu(self)
+        self.uploader_entry.grid(column=2, row=2, sticky="we")
 
     def __setattr__(self, key, value):
         if key == "thumbnail":
@@ -96,14 +85,14 @@ class Previewer_frame(Frame):
                 img = Image.open(data).resize((128, 72), Image.ANTIALIAS)
                 self.image = ImageTk.PhotoImage(img)
             self.thumbnail_frame["image"] = self.image
-        elif key in ("title", "uploader", "url"):
+        elif key in ("url", "title", "uploader"):
             getattr(self, f"{key}_entry").set(value)
         else:
             super().__setattr__(key, value)
 
 class Tree(ttk.Treeview):
-    def __init__(self, parent):
-        self.frame = Frame(parent)
+    def __init__(self, parent, headers):
+        self.frame = ttk.Frame(parent)
         self.frame.columnconfigure(0, weight=1)
         self.frame.rowconfigure(0, weight=1)
 
@@ -120,17 +109,13 @@ class Tree(ttk.Treeview):
 
         ttk.Style().layout("Treeview", [("treeview.treearea", {"sticky": "nswe"})])
 
-        self["columns"] = ["Title", "Progress", "ETA", "Speed"]
-        self.heading("#0", text="Uploader", anchor="w")
-        self.column("#0", stretch=0, anchor="w", minwidth=100, width=100)
+        self["columns"], widths = zip(*headers[1:])
+        self.heading("#0", text=headers[0][0], anchor="w")
+        self.column("#0", stretch=0, anchor="w", minwidth=headers[0][1], width=headers[0][1])
 
-        for i, w in zip(self["columns"][:-1], (190, 70, 50)):
+        for i, w in headers[1:]:
             self.heading(i, text=i, anchor="w")
             self.column(i, stretch=0, anchor="w", minwidth=w, width=w)
-
-        i = self["columns"][-1]
-        self.heading(i, text=i, anchor="w")
-        self.column(i, stretch=1, anchor="w", minwidth=70, width=70)
 
     def grid(self, **kwargs):
         self.frame.grid(**kwargs)
@@ -144,66 +129,126 @@ class Tree(ttk.Treeview):
             sbar.grid()
         sbar.set(first, last)
 
-class Bottom(Frame):
+    def cancel(self):
+        print("tv cancel")
+
+    def pause(self):
+        print("tv pause")
+
+    def download_speed(self):
+        print("tv download_speed")
+
+class BottomFrame(ttk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
 
-        self.cb = ttk.Combobox(self, values=["Audio", "Video"])
-        self.cb.set("Audio")
-        self.cb.grid(padx=5, pady=5, sticky="nw")
+        self.cmb = ttk.Combobox(self, values=["Audio", "Video"])
+        self.cmb.set("Audio")
+        self.cmb.grid(padx=5, pady=5, sticky="nw")
 
         self.download_btn = ttk.Button(self, text="Download", command=self.download)
         self.download_btn.grid(column=1, row=0, padx=5, pady=5, sticky="nw")
 
     def download(self):
-        self.master.add(self.cb.get())
+        self.master.download(self.cmb.get())
 
-class main(Tk):
+class Main(Tk):
     def __init__(self):
         super().__init__()
 
-        self.settings = {}
-        self.pv = Preview(self.populate_previewer)
-        self.dc = Downloader(self.handle_download)
-        self.current_info = {"status": None}
-
-        try:
-            self.current_url = self.clipboard_get()
-        except TclError:
-            self.current_url = None
+        self.state = {
+            "current_url": None
+        }
+        self.settings = {
+            "geometry": "600x250+400+300",
+            "treeview": [
+                ["Uploader", 100],
+                ["Title", 190],
+                ["Progress", 70],
+                ["ETA", 50],
+                ["Speed", 70]
+            ]
+        }
 
         try:
             with open("settings.json") as fp:
-                self.settings = json.load(fp)
+                self.settings.update(json.load(fp))
         except FileNotFoundError:
             with open("settings.json", "w") as fp:
                 json.dump(self.settings, fp)
 
-        self.title("Youtube Downloader")
+        self.title("Video Downloader")
         self.attributes("-topmost", True)
-        self.geometry(self.settings.get("geometry", "600x250+400+300"))
+        self.geometry(self.settings["geometry"])
         self.columnconfigure(0, weight=1)
         self.rowconfigure(1, weight=1)
         self.minsize(600, 250)
 
-        self.previewer_frame = Previewer_frame(self)
-        self.previewer_frame.grid(padx=5, pady=5, sticky="nwe")
+        self.preview_frame = PreviewFrame(self)
+        self.preview_frame.grid(padx=5, pady=5, sticky="nwe")
 
-        self.tv = Tree(self)
+        self.tv = Tree(self, self.settings["treeview"])
         self.tv.grid(column=0, row=1, padx=5, pady=5, sticky="nswe")
 
-        self.bottom = Bottom(self)
-        self.bottom.grid(column=0, row=2, sticky="w")
+        self.bottom_frame = BottomFrame(self)
+        self.bottom_frame.grid(column=0, row=2, sticky="w")
+
+        self.menu = PopupMenu(self, "cut", "copy", "paste")
+        self.tv_menu = PopupMenu(self, "cancel", "pause", "download_speed")
+
+        self.pv_thread = downloader.Preview(self.callback)
+        self.dl_thread = downloader.Downloader(self.callback)
 
         try:
-            self.pv.add(self.clipboard_get())
+            self.pv_thread.add(self.clipboard_get())
         except TclError:
             pass
-            
+
+        self.bind("<Button-3>", self.popup)
+
         self.after(100, self.check_clipboard)
 
         self.protocol("WM_DELETE_WINDOW", self.end)
         self.mainloop()
+
+    def download(self, filetype):
+        info = self.state["current_info"]
+
+        if info["status"] == "Ok" and not self.tv.exists(info["id"]):
+            info["filetype"] = filetype
+            info["title"] = self.preview_frame.title_entry.get()
+            info["uploader"] = self.preview_frame.uploader_entry.get()
+
+            values = info["title"], "Queued", "-", "-"
+            self.tv.insert("", "end", info["id"], text=info["uploader"], values=values)
+            self.dl_thread.add(info)
+
+    def callback(self, info):
+        if info["status"] == "Downloading":
+            remaining = info["length"] - info["progress"]
+            eta = f"{remaining / info['speed']:.2f}"
+
+            if info["speed"] < 2e3:
+                speed = f"{info['speed']:.0f}bps"
+            elif info["speed"] < 2e6:
+                speed = f"{info['speed']/1e3:.0f}Kbps"
+            elif info["speed"] < 2e9:
+                speed = f"{info['speed']/1e6:.0f}Mbps"
+            elif info["speed"] < 2e12:
+                speed = f"{info['speed']/1e9:.0f}Gbps"
+
+            values = info["title"], f"{info['progress']*100/info['length']:.2f}%", eta, speed
+
+            self.tv.item(info["id"], values=values)
+        elif info["status"] == "Finished":
+            self.tv.delete(info["id"])
+        else:
+            self.preview_frame.url = info["url"]
+            self.preview_frame.thumbnail = info["thumbnail"]
+            self.preview_frame.title = info["title"]
+            self.preview_frame.uploader = info["uploader"]
+
+            self.state["current_info"] = info
 
     def check_clipboard(self):
         try:
@@ -211,32 +256,25 @@ class main(Tk):
         except TclError:
             pass
         else:
-            if self.current_url != url:
-                self.current_url = url
+            if self.state["current_url"] != url:
+                self.state["current_url"] = url
 
                 if self.focus_get() is None:
-                    self.pv.add(self.current_url)
+                    self.pv_thread.add(self.state["current_url"])
 
             self.after(100, self.check_clipboard)
 
-    def add(self, download_type):
-        if self.current_info["status"] == "Ok":
-            self.current_info["type"] = download_type
-            self.dc.add(self.current_info)
-
-    def handle_download(self, info):
-        print(info)
-
-    def populate_previewer(self, info):
-        self.previewer_frame.url = info["url"]
-        self.previewer_frame.thumbnail = info["thumbnail"]
-        self.previewer_frame.title = info["title"]
-        self.previewer_frame.uploader = info["uploader"]
-
-        self.current_info = info
+    def popup(self, args):
+        if args.widget == self.tv:
+            self.tv_menu.post(args.widget, args.x_root, args.y_root)
+        elif args.widget in (self.preview_frame.title_entry, self.preview_frame.uploader_entry):
+            self.menu.post(args.widget, args.x_root, args.y_root)
 
     def end(self):
         self.settings["geometry"] = self.geometry()
+        
+        for n, header in enumerate(self.settings["treeview"]):
+            header[1] = self.tv.column(f"#{n}", "width")
 
         with open("settings.json", "w") as fp:
             json.dump(self.settings, fp)
@@ -244,4 +282,4 @@ class main(Tk):
         self.destroy()
 
 if __name__ == "__main__":
-    main()
+    Main()
