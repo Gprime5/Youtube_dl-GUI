981e91c73fed2eecea452eb526c587f5ff25b690
Gprime <gprime@hotmail.co.uk>
2020-01-12 02:51:16 +0000

    Complete refactoring

--- downloader.py
+++ downloader.py
@@ -1,3 +1,4 @@
+from io import BytesIO
 from queue import Queue
 from threading import Thread
 import json
@@ -7,6 +8,7 @@ import subprocess
 import time
 
 from youtube_dl import YoutubeDL, utils
+import mutagen
 import requests
 
 session = requests.Session()
@@ -19,45 +21,45 @@ logging.basicConfig(
 
 os.makedirs("Downloads", exist_ok=True)
 
-class Preview(Thread):
+class BaseThread(Queue, Thread):
     def __init__(self, callback):
-        super().__init__(daemon=True)
+        Queue.__init__(self)
+        Thread.__init__(self, daemon=True)
 
-        self.queue = Queue()
         self.callback = callback
-        self.ytd = YoutubeDL({
-            "logger": logging,
-            "progress_hooks": [self.callback],
-            "format": "mp4"
-        })
-
         self.start()
-
-    def add(self, item):
-        self.queue.put(item)
 
     def run(self):
         while True:
-            self.check(self.queue.get())
+            self.process(self.get())
 
-    def check(self, url):
+class Preview(BaseThread):
+    def __init__(self, callback):
+        self.ytd = YoutubeDL({
+            "logger": logging,
+            "progress_hooks": [callback]
+        })
+        super().__init__(callback)
+
+    def process(self, url):
         if "youtube" in url:
             url = url.split("&")[0]
         elif "youtu.be" in url:
             yt_id = url.split("?")[0].split("/")[-1]
             url = f"https://www.youtube.com/watch?v={yt_id}"
 
+        self.callback({
+            "status": "Extracting",
+            "url": url,
+            "thumbnail": None,
+            "title": "Extracting",
+            "uploader": "Extracting info"
+        })
+
         try:
-            self.callback({
-                "status": "Extracting",
-                "url": url,
-                "thumbnail": None,
-                "title": "Extracting",
-                "uploader": "Extracting info"
-            })
             result = self.ytd.extract_info(url, process=False)
         except utils.DownloadError as e:
-            if "is not a valid URL." in e.args[0]:
+            if "is not valid URL." in e.args[0]:
                 self.callback({
                     "status": "Error",
                     "url": url,
@@ -66,11 +68,11 @@ class Preview(Thread):
                     "uploader": "Invalid url"
                 })
         else:
+            # TODO: Add playlist functionality
             # if result.get("_type") == "playlist":
             #     base = "https://www.youtube.com/watch?v="
             #     self.check(base + next(result["entries"])["id"])
             # else:
-            # TODO: Add playlist functionality
 
             max_video = max_audio = {}
 
@@ -89,106 +91,76 @@ class Preview(Thread):
                 "id": result["id"],
                 "title": result["title"],
                 "uploader": result["uploader"],
-                "thumbnail": result["thumbnail"],
+                "thumbnail": BytesIO(session.get(result["thumbnail"]).content),
                 "best_video": max_video,
                 "best_audio": max_audio or max_video
             })
 
-class Downloader(Thread):
-    def __init__(self, callback):
-        super().__init__(daemon=True)
-
-        self.queue = Queue()
-        self.callback = callback
-        self.pending_removal = set()
-
-        self.start()
-
-    def add(self, info):
-        self.queue.put(info)
-
-    def run(self):
-        while True:
-            self.download(self.queue.get())
-
-    def remove(self, tv_id):
-        self.pending_removal.add(tv_id)
-
-    def download(self, info):
-        if info["id"] in self.pending_removal:
-            self.pending_removal.remove(info["id"])
-            return
-
-        info["status"] = "Downloading"
-
-        # Maybe just use youtube_dl's downloader
-
-        data = bytearray()
-        previous_time = time.time()
-        start, end = 0, 1024 * 1024 - 1
+class Downloader(BaseThread):
+    def process(self, info):
         filetype = f"best_{info['filetype'].lower()}"
+        filename = f"Downloads/{info['title']}.{info[filetype]['ext']}"
 
-        while True:
-            session.headers["range"] = f"bytes={start}-{end}"
+        if not os.path.isfile(filename):
+            info["status"] = "Downloading"
 
-            response = session.get(info[filetype]["url"])
-
-            if response.ok:
-                data += response.content
-            else:
-                break
-
-            content_range = response.headers["Content-Range"].split("/")
-
-            info["speed"] = len(response.content) / (time.time() - previous_time)
-            info["progress"] = len(data)
-            info["length"] = int(content_range[1])
-
-            if int(content_range[0].split("-")[1]) + 1 == info["length"]:
-                break
-
-            if info["id"] in self.pending_removal:
-                self.pending_removal.remove(info["id"])
-                return
-
-            self.callback(info)
-
+            data = bytearray()
             previous_time = time.time()
-            start += 1024 * 1024
-            end += 1024 * 1024
+            start, end = 0, 2**20 - 1
 
-        with open(f"Downloads/{info['title']}.{info[filetype]['ext']}", "wb") as fp:
-            fp.write(data)
+            while True:
+                session.headers["range"] = f"bytes={start}-{end}"
+
+                response = session.get(info[filetype]["url"])
+
+                if response.ok:
+                    data += response.content
+                else:
+                    break
+
+                content_range = response.headers["Content-Range"].split("/")
+
+                info["speed"] = len(response.content) / (time.time() - previous_time)
+                info["progress"] = len(data)
+                info["length"] = int(content_range[1])
+                if self.callback(info):
+                    return
+
+                if int(content_range[0].split("-")[1]) + 1 == info["length"]:
+                    break
+
+                previous_time = time.time()
+                start += 2**20
+                end += 2**20
+
+            with open(filename, "wb") as fp:
+                fp.write(data)
 
         info["status"] = "Finished"
         self.callback(info)
 
-class Converter(Thread):
-    def __init__(self, callback):
-        super().__init__(daemon=True)
-
-        self.queue = Queue()
-        self.callback = callback
-
-        self.start()
-
-    def add(self, info):
-        self.queue.put(info)
-
-    def run(self):
-        while True:
-            self.convert(self.queue.get())
-
-    def convert(self, info):
-        info["status"] = "Converting"
-        self.callback(info)
+class Converter(BaseThread):
+    def process(self, info):
         ext = info[f"best_{info['filetype'].lower()}"]['ext']
-        subprocess.run([
-            "ffmpeg.exe",
-            "-i", f"Downloads/{info['title']}.{ext}",
-            f"Downloads/{info['title']}.mp3",
-            "-y"
-        ])
+        old_filename = f"Downloads/{info['title']}.{ext}"
+        new_filename = f"Downloads/{info['title']}.mp3"
+
+        if not os.path.isfile(new_filename):
+            info["status"] = "Converting"
+            self.callback(info)
+
+            subprocess.run([
+                "ffmpeg.exe",
+                "-i", old_filename,
+                new_filename,
+                "-y"
+            ])
+
+            os.remove(old_filename)
+
+            muta = mutagen.File(new_filename, easy=True)
+            muta["artist"] = info["uploader"]
+            muta.save()
+
         info["status"] = "Converted"
-        os.remove(f"Downloads/{info['title']}.{ext}")
         self.callback(info)
